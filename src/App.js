1ede93a6bbd6a9a5767bf51e3d78594f3b4f52df
sung yong <sssssqew@gmail.com>
2019-02-27 18:10:42 +0900

    초기 커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetails from './components/video_detail';
+
+const API_KEY = 'AIzaSyAKHUVkCLJ_AatB1R1dzftgh9LFknGG6xM';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('');
+  }
+
+  videoSearch(term){
+    YTSearch({ key: API_KEY, term: term }, (data) => {
+      console.log(data);
+      this.setState({
+        videos: data,
+        selectedVideo: data[0]
+      });
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <SearchBar onSearchTermChange={searchTerm=> this.videoSearch(searchTerm)}/> {/* callback */}
+        <div style={{ "display" : "flex" }}>
+           <VideoDetails video={this.state.selectedVideo}/>
+          <VideoList
+          onVideoSelect={userSelected => this.setState({ selectedVideo: userSelected})} // callback
+          videos={this.state.videos}/>
+        </div>
       </div>
     );
   }
